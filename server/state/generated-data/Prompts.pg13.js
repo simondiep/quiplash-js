672f2701598e40af3f61958a6cb9642e11e60472
Kathryn Tsai <kathryntsai@users.noreply.github.com>
2023-03-30 14:22:21 -0400

    Update Prompts.pg13.js

--- server/state/generated-data/Prompts.pg13.js
+++ server/state/generated-data/Prompts.pg13.js
@@ -12,9 +12,9 @@ const prompts = [
   "Tristan has lost his water bottle again.  Where did he leave it this time?",
   "Tristan’s childhood email was penguinpuppy0@gmail.com.  What animal is he most similar to?",
   "Tristan’s undergrad major was chemical engineering. Name Tristan\u2019s favorite element on the periodic table.",
-  "If Tristan weren\u2019t going to be a doctor, what else would he be doing with his life?",
+  "If Tristan weren\u2019t a doctor, what else would he be doing with his life?",
   "Insert Tristan into a TV series. What would it be?",
-  "Tristan is from Houston, Texas. What would he recommend you wear in a visit to his hometown?",
   "What would Tristan's superhero name be?",
-  "If he could choose, what would Tristan's superpower be?",
+  "If he could choose, what would Tristan's superpower be?", 
+  /* something Houston-related? */
 ];
